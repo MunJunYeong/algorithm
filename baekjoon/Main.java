9b5a02dea6385aee8138cb5711affa4e95c91391
DESKTOP-G145FC7\moon <msw711666@naver.com>
2021-12-25 01:50:45 +0900

    BigInteger

--- baekjoon/Main.java
+++ baekjoon/Main.java
@@ -1,22 +1,18 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args)   {
+	public static void main(String[] args) throws IOException   {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String s = br.readLine();
+		br.close();
+		StringTokenizer st = new StringTokenizer(s);
+		BigInteger a = new BigInteger(st.nextToken());
+		BigInteger b = new BigInteger(st.nextToken());
+		System.out.println(a.add(b));
 		
-		Scanner sc = new Scanner(System.in);
-		int num = sc.nextInt();
-		sc.close();
-		
-		if(num == 4 || num == 7) {
-			System.out.println(-1);
-		}else if(num%5 == 0) {
-			System.out.println(num/5);
-			
-		}else if(num%5 ==1 || num%5 == 3) {
-			System.out.println( (num/5) + 1);
-			
-		}else if(num%5 == 2 || num%5 ==4) {
-			System.out.println( (num/5) + 2);
-		}
 	}
 }
