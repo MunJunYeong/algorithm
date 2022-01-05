41033f86421b26439ad982a526f29ac76e4dd525
MunJunYeong <>
2022-01-05 15:52:06 +0900

    repeat chap6 function

--- repeat/c6/a3.java
+++ repeat/c6/a3.java
@@ -1,11 +1,10 @@
+package c6;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
-public class Main {
-	
-	
+public class a3 {
 	public static void main(String[] args) throws IOException   {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int num = Integer.parseInt(br.readLine());
@@ -37,8 +36,4 @@ public class Main {
 		
 		return count;
 	}
-	
-	
-	
 }
-
---
+++ repeat/c7/a1.java
@@ -0,0 +1,24 @@
+package c7;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class a1 {
+	
+	public static void main(String[] args) throws IOException   {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int num = Integer.parseInt(br.readLine());
+		int  arr[] = new int[num];
+		String s = br.readLine();
+		StringTokenizer st =new StringTokenizer(s);
+		for(int i =0; i < num; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(arr);
+		System.out.println(arr[0] + " " + arr[num-1]);
+		
+	}
+}
