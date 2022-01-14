320ab27a947b871dfc5298f98424382a948c0504
MunJunYeong <>
2022-01-14 13:37:40 +0900

    greedy solve 1 problem

--- baekjoon/greedy/B16953.java
+++ baekjoon/greedy/B16953.java
@@ -1,11 +1,11 @@
+package greedy;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
-	
+public class B16953 {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
