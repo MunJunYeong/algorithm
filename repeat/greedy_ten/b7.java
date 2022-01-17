cf549018eeb8b76004af397ece4c25a9f1a9eb75
MunJunYeong <>
2022-01-17 15:15:32 +0900

    solve greedy problem repeat 14 to 20

--- repeat/greedy_ten/b7.java
+++ repeat/greedy_ten/b7.java
@@ -1,5 +1,37 @@
 package greedy_ten;
 
-public class b7 {
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class b7 {
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+	
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int cnt = 1;
+		while(true) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int day = Integer.parseInt(st.nextToken());
+			int p = Integer.parseInt(st.nextToken());
+			int total = Integer.parseInt(st.nextToken());
+			if(day == 0) {
+				break;
+			}
+			int res = (total/p) * day;
+			int nmg = total%p;
+			if(nmg > day) {
+				res = day + ((total/p)*day);
+			}else {
+				res = nmg + ((total/p)*day);
+			}
+			
+			System.out.println("Case "+ cnt+ ": " + res);
+			cnt ++;
+		}
+		
+		
+	}
 }
