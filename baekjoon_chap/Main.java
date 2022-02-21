2fb262fe345a0493e97a2f25a3afb474d202fe23
DESKTOP-G145FC7\moon <msw711666@naver.com>
2022-02-22 00:42:40 +0900

    update

--- baekjoon_chap/Main.java
+++ baekjoon_chap/Main.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
@@ -15,71 +16,65 @@ class Dot{
 }
 
 public class Main {
-	static int N;
-	static int K;
+	static int w;
+	static int h;
 	
 	static int map[][];
+	static boolean visit[][];
 	
-	static int [] xArr = {-1, 1, 0, 0};
-	static int[] yArr = {0,0, -1, 1};
+	static int [] xArr = {-1, -1, -1, 0, 0, 1 , 1, 1};
+	static int[] yArr = {-1, 0, 1, -1, 1, -1, 0, 1};
 	
-	static int check[] = new int[100001];
-	
+	static ArrayList<Integer> arr = new ArrayList<Integer>();
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st=  new StringTokenizer(br.readLine());
-		
-		N = Integer.parseInt(st.nextToken()); //subin
-		K = Integer.parseInt(st.nextToken()); //sister
-		
-		if(N == K) {
-			System.out.println(0);
-		}else {
-			bfs(N);
-		}
-		
-		
-	}
-
-	private static void bfs(int n) {
-		Queue<Integer> q = new LinkedList<Integer>();
-		q.offer(n);
-		check[n] = 1;
-		
-		while(!q.isEmpty()) {
-			int qPoll = q.poll();
-			for(int i = 0; i < 3; i++) {
-				int next;
-				if(i == 0) {
-					next = qPoll+1;
-				}else if(i == 1) {
-					next = qPoll-1;
-				}else {
-					next = qPoll * 2;
+		while(true) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			w = Integer.parseInt(st.nextToken());
+			h = Integer.parseInt(st.nextToken());
+			if(w == 0 && h == 0) break;
+			map = new int[h+1][w+1];
+			visit = new boolean[h+1][w+1];
+			for(int i = 1; i < h+1; i++) {
+				StringTokenizer st1 = new StringTokenizer(br.readLine());
+				for(int j =1; j < w+1; j++) {
+					map[i][j] = Integer.parseInt(st1.nextToken());
 				}
-				
-				if(next == K) {
-					System.out.println(check[qPoll]); return;
-				}
-				if(next >= 0 && next < check.length && check[next] ==0) {
-					q.add(next);
-					check[next] = check[qPoll] +1;
-				}
-				
-				
 			}
-			
+			int cnt =0;
+			for(int i= 1; i < h+1; i++) {
+				for(int j =1; j < w+1; j++) {
+					if(map[i][j] == 1 && !visit[i][j]) {
+						dfs(i , j);
+						cnt++;
+					}
+				}
+			}
+			System.out.println(cnt);
 			
 		}
-		
 	}
-
-
-
-
-
-	
-	
-	
+	public static void dfs(int a , int b) {
+		visit[a][b] = true;
+		Queue<Dot> q = new LinkedList<Dot>();
+		q.offer(new Dot(a, b));
+		while(!q.isEmpty()) {
+			Dot d = q.poll();
+			for(int i = 0; i < 8 ; i++) {
+				int dx = d.x + xArr[i];
+				int dy = d.y + yArr[i];
+				if(dx >= 0 && dy >= 0 && dx < h+1 && dy < w+1) {
+					if(map[dx][dy] == 1 && !visit[dx][dy]) {
+						q.offer(new Dot(dx, dy));
+						visit[dx][dy] =true;
+					}
+				}
+			}
+		}
+	}
 
 }
+
+	
+	
+
