a263992df47295d31fab1e982ab317a91c594ca3
DESKTOP-G145FC7\moon <msw711666@naver.com>
2022-03-02 22:44:58 +0900

    repeat graph

--- baekjoon_chap/Main.java
+++ baekjoon_chap/Main.java
@@ -1,59 +1,89 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
+class Cab {
+	int x;
+	int y;
+	public Cab(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+}
 
 public class Main {
-	static int N, M;
+	static int N, M, K;
+	static int vertex, edge;
 	
 	static int arr[][];
-	static boolean visit[] = new boolean[26];
-	
+	static boolean visit[][];
 	static int[] xArr = {-1, 1, 0, 0};
 	static int[] yArr = {0, 0, -1, 1};
 	
-	static int res=0;
+	static int res = 0;
+	
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		// N 세로 , M 가로
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
 		
-		arr = new int[N][M];
-		
-		for(int i = 0; i < N; i++) {
-			String s = br.readLine();
-			for(int j = 0; j < M; j++) {
-				char c = s.charAt(j);
-				arr[i][j] = c - 'A'; 
+		int test = Integer.parseInt(br.readLine());
+		while(test -- > 0) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			M = Integer.parseInt(st.nextToken());
+			N = Integer.parseInt(st.nextToken());
+			K = Integer.parseInt(st.nextToken()); // 배추 개수
+			
+			arr = new int[N][M];
+			visit = new boolean[N][M];
+			
+			for(int i = 0; i < K ; i++) {
+				StringTokenizer st2 = new StringTokenizer(br.readLine());
+				int a= Integer.parseInt(st2.nextToken());
+				int b= Integer.parseInt(st2.nextToken());
+				arr[b][a]  = 1;
 			}
-		}
-		
-		dfs(0, 0, 0);
-		System.out.println(res);
-	}
-
-
-	private static void dfs(int x, int y, int cnt) {
-		if(visit[arr[x][y]]) {
-			res =Math.max(res, cnt);
-			return;
-		}else {
-			visit[arr[x][y]] =  true;
-			for(int i = 0; i < 4; i++) {
-				int dx = x + xArr[i];
-				int dy = y + yArr[i];
-				if(dx >= 0 && dy >= 0 && dx < N && dy < M) {
-					dfs(dx, dy, cnt +1);
+			int cnt =0;
+			for(int i = 0 ; i < N ; i++) {
+				for(int j = 0 ; j < M ; j++) {
+					if(!visit[i][j] && arr[i][j] == 1 ) {
+						bfs(i, j);
+						cnt++;
+					}
 				}
 			}
-			visit[arr[x][y]]= false;
+			System.out.println(cnt);
+			for(boolean temp[] : visit) {
+				Arrays.fill(temp, false);
+			}
 		}
+		
+		
 	}
 
 
+	private static void bfs(int x, int y) {
+		Queue<Cab> q = new LinkedList<Cab>();
+		visit[x][y] = true;
+		q.add(new Cab(x, y));
+		while(!q.isEmpty()) {
+			Cab d = q.poll();
+			for(int i =0; i < 4; i++) {
+				int dx = d.x + xArr[i];
+				int dy = d.y + yArr[i];
+				if(dx >=0 && dy >= 0 && dx < N && dy < M) {
+					if(!visit[dx][dy] && arr[dx][dy] == 1) {
+						visit[dx][dy] = true;
+						q.add(new Cab(dx, dy));
+					}
+				}
+			}
+		}
+	}
 }
 
 	
--- repeat/graph/a2.java
+++ repeat/graph/a2.java
@@ -1,90 +1,55 @@
 package graph;
 
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.Queue;
-
-class b2667Location{
-	int x;
-	int y;
-	public b2667Location(int x, int y) {
-		this.x= x;
-		this.y= y;
-	}
-}
+import java.util.StringTokenizer;
 
 public class a2 {
-	static int N;
+	static int N, M;
+	static int vertex, edge;
 	
-	static int map[][];
-	static boolean visit[][];
+	static int arr[][];
+	static boolean visit[];
+	static int[] xArr = {-1, 1, 0, 0};
+	static int[] yArr = {0, 0, -1, 1};
 	
-	static int [] xArr = {1, -1, 0, 0};
-	static int[] yArr = {0,0, -1, 1};
-	
-	static ArrayList<Integer> arr = new ArrayList<Integer>();
-	static int cnt = 0; 
+	static int res = 0;
 	
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		N = Integer.parseInt(br.readLine());
-		
-		map = new int[N][N];
-		visit = new boolean[N][N];
-		
-		for(int i = 0; i < N; i ++) {
-			String s = br.readLine();
-			for(int j = 0; j < N ; j++) {
-				map[i][j] = s.charAt(j) - '0';
-			}
-		}
-		
-		for(int i = 0 ; i < N ; i++) {
-			for(int j = 0; j < N ; j++) {
-				if(map[i][j] == 1 && !visit[i][j]) {
-					cnt = 1;
-					bfs(i, j);
-					arr.add(cnt);
-				}
-			}
-		}
-		System.out.println(arr.size());
-		Collections.sort(arr);
-		for(int i = 0 ; i < arr.size(); i++) {
-			System.out.println(arr.get(i));
-		}
-	}
 
-	private static void bfs(int x, int y) {
-		Queue<b2667Location> q = new LinkedList<>();
-		q.offer(new b2667Location(x, y));
-		visit[x][y] = true;
+		vertex = Integer.parseInt(br.readLine());
+		edge = Integer.parseInt(br.readLine());
 		
-		while(!q.isEmpty()) {
-			b2667Location l = q.poll();
-			for(int i = 0; i < 4; i++) {
-				int dx = l.x + xArr[i];
-				int dy = l.y + yArr[i];
-				if(dx >=0 && dy >= 0 && dx < N && dy < N) {
-					if(map[dx][dy] == 1 && !visit[dx][dy]) {
-						visit[dx][dy] = true;
-						q.offer(new b2667Location(dx, dy));
-						cnt ++;
-					}
-				}
+		arr = new int[vertex+1][vertex+1];
+		visit = new boolean[vertex+1];
+		
+		for(int i = 0; i < edge ; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			arr[a][b] = 1;
+			arr[b][a] = 1;
+		}
+		dfs(1);
+		
+		System.out.println(res);
+	}
+	private static void dfs(int v) {
+		visit[v] = true;
+		for(int i = 1; i < vertex+ 1; i++) {
+			if(!visit[i] && arr[v][i] == 1) {
+				res++;
+				dfs(i);
 			}
 		}
 		
 	}
-	
-	
-	
-	
-	
-
 }
+
+	
+	
+
+
