24561fec742c26b47f472c4a42cbd887395609d7
MunJunYeong <>
2022-03-23 14:35:57 +0900

    repeat dp 2

--- repeat/dp/a7.java
+++ repeat/dp/a7.java
@@ -1,11 +1,12 @@
+package dp;
+
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 
-public class Main {
+public class a7 {
 
 	static int[] dp;
 	static int arr[];
@@ -15,27 +16,26 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		int n = Integer.parseInt(br.readLine());
-		arr = new int[n];
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		for(int i = 0; i < n; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
+		arr = new int[n+1];
+		for(int i = 1; i <= n; i ++) {
+			arr[i] = Integer.parseInt(br.readLine());
 		}
-		dp = new int[n];
+		dp = new int[n+1];
+		dp[1] = arr[1];
 		
-		for(int i = 0; i < n; i++) {
-			dp[i] = 1;
-			for(int j = 0; j < i; j++) {
-				if(arr[i] > arr[j] && dp[i] < dp[j]+1) {
-					dp[i] = dp[j+1];
-				}
-			}
+		if(n >=2) {
+			dp[2] = arr[2]+ arr[1];
 		}
 		
-		
+		for(int i = 3; i < n+1; i++) {
+			dp[i] = arr[i] + Math.max(dp[i-2], dp[i-3]+arr[i-1]);
+		}
+		System.out.println(dp[n]);
 		
 	}
 }
 
 	
 	
+
 
