bbb2312f055dd70d9b7d05bf0d41541206feca85
MunJunYeong <>
2022-04-09 15:13:22 +0900

    solve greedy 3

--- baekjoon_type/greedy/b21314.java
+++ baekjoon_type/greedy/b21314.java
@@ -1,10 +1,12 @@
+package greedy;
+
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 
-public class Main {
+public class b21314 {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
