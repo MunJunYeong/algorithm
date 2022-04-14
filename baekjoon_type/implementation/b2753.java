16ff013d08402114ef67a3e1b8533e52efcd1d41
DESKTOP-G145FC7\moon <msw711666@naver.com>
2022-04-14 22:54:30 +0900

    start implementation

--- baekjoon_type/implementation/b2753.java
+++ baekjoon_type/implementation/b2753.java
@@ -1,9 +1,11 @@
+package implementation;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 
-public class Main {
+public class b2753 {
 	
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
