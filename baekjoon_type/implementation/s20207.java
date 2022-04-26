eb699eaf3d5fa70f90a5a7adfe712ff533426348
MunJunYeong <>
2022-04-26 20:02:37 +0900

    implementation

--- baekjoon_type/implementation/s20207.java
+++ baekjoon_type/implementation/s20207.java
@@ -1,13 +1,12 @@
+package implementation;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
 import java.util.StringTokenizer;
 
 
-public class Main {
+public class s20207 {
 	
 	static int n, s, e;
 	static int[][] arr;
