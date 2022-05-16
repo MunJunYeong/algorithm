dddd4ab761bc79a277aaed3d966609c28fa7d3c9
MunJunYeong <>
2022-05-16 18:01:01 +0900

    solve dp repeat 1, now 1

--- baekjoon_type/dp/s11055.java
+++ baekjoon_type/dp/s11055.java
@@ -1,11 +1,12 @@
+package dp;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 
-public class Main {
+public class s11055 {
 	
 	static int s, n;
 	static int[] dp;
