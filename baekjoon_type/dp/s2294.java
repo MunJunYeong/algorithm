301955831e947eed5827c95aa11d0d25bd6dcfaa
MunJunYeong <>
2022-05-24 20:37:46 +0900

    solve dp 2

---
+++ baekjoon_type/dp/s2294.java
@@ -0,0 +1,43 @@
+package dp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+
+public class s2294 {
+	
+	static int n, k;
+	static int[] arr;
+	static int[] dp;
+	static int max = Integer.MAX_VALUE-1;
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st =new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		k = Integer.parseInt(st.nextToken());
+		arr = new int[n];
+		dp = new int[k+1];
+		
+		for(int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+		for(int i = 1; i <=k;i++) {
+			dp[i] = max;
+		}
+		
+		for(int i =0 ; i<n; i++) {
+			for(int j = arr[i]; j <=k; j++) {
+				dp[j] = Math.min(dp[j], dp[j-arr[i]] +1);
+			}
+		}
+		if(dp[k] == max) {
+			System.out.println(-1);
+		}else {
+			System.out.println(dp[k]);
+		}
+	}
+
+}
